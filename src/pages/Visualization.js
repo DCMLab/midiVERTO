80e602839e98f03bd7d7b5ed854d2696908696bf
AyTaken <39062949+AyTaken@users.noreply.github.com>
2021-08-12 17:41:51 +0200

    Added d3.js, math.js packages and DFT logic

--- src/pages/Visualization.js
+++ src/pages/Visualization.js
@@ -1,4 +1,27 @@
 import React, { useState } from 'react';
+import { multiply, add, pi, i, exp, abs } from 'mathjs';
+
+//SET CLASSES
+let setClasses = [
+  // C  C# D  D# E  F  F# G  G# A  Bb B
+  { name: 'Single Tone', pcv: [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] }, //Single tone
+  { name: 'Tritone', pcv: [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0] }, //Tritone
+  { name: 'Major Triad', pcv: [1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0] }, //Major triad
+  { name: 'Aug Triad', pcv: [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0] }, //Aug triad
+  { name: 'Maj7', pcv: [1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1] }, //Maj7
+  { name: 'Min7', pcv: [1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0] }, //min7
+  { name: 'Half-Dim7', pcv: [1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0] }, //Half-dim 7
+  { name: 'Dim7', pcv: [1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0] }, //dim 7
+  { name: 'Pentatonic', pcv: [1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0] }, //Pentatonic
+  { name: "Guido's Hexachord", pcv: [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0] }, //Guido's hexachord
+  { name: 'Whole-Tone Scale', pcv: [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0] }, //Whole-tone scale
+  { name: '6 chromatic Tones6', pcv: [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0] }, //6 chromatic tones
+  { name: 'Diatonic Scale', pcv: [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1] }, //Diatonic scale
+  { name: '3 chromatic tritones', pcv: [1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0] }, //3 chromatic tritones
+  { name: 'Hexatonic Scale', pcv: [1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1] }, //Hexatonic scale
+  { name: 'Octatonic Scale', pcv: [1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1] }, //Octatonic scale
+  { name: 'All Tones', pcv: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1] }, //All tones
+];
 
 export default function Visualization() {
   const [pitchClass, setPitchClass] = useState('');
@@ -38,6 +61,62 @@ export default function Visualization() {
         </div>
       </form>
       <h1>The application goes here</h1>
+      <div id='chart'></div>
     </>
   );
+}
+
+// DFT function
+function dft(pcv) {
+  let N = pcv.length;
+
+  let coeff = [];
+
+  for (let k = 0; k < N; k++) {
+    let temp = 0;
+    for (let n = 0; n < N; n++) {
+      temp = add(
+        multiply(pcv[n], exp(multiply(i, (-2 * pi * k * n) / N))),
+        temp
+      );
+    }
+    coeff.push(temp);
+  }
+  return coeff.slice(0, 7);
+}
+
+let pcvs = setClasses.map((setClass) => setClass.pcv);
+let names = setClasses.map((setClass) => setClass.name);
+let magn = [];
+let spect = [];
+
+// Compute the dft coeffs
+for (let i = 0; i < pcvs.length; i++) {
+  let coeffs = dft(pcvs[i]);
+  spect.push(coeffs.slice());
+}
+
+// Compute the coeffs' magnitude
+magn.push(abs(spect));
+magn = magn[0];
+
+// Normalization
+for (let i = 0; i < magn.length; i++) {
+  let norm = abs(magn[i][0]);
+
+  for (let j = 0; j < magn[i].length; j++) magn[i][j] = magn[i][j] / norm;
+}
+
+// Rounding
+for (let i = 0; i < magn.length; i++) {
+  for (let j = 0; j < magn[i].length; j++)
+    magn[i][j] = Math.round(magn[i][j] * 100) / 100;
+}
+
+// Printing
+let printableMagn = [];
+for (let i = 0; i < magn.length; i++) {
+  printableMagn.push(magn[i].slice(1, 7));
+  console.log(names[i] + ':\n');
+  console.log(printableMagn[i]);
 }
