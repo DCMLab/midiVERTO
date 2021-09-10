ef0430507a9101f15c3325dc1d0b5c6984909b9a
AyTaken <39062949+AyTaken@users.noreply.github.com>
2021-09-10 11:45:01 +0200

    Test 3 fix gh-pages react router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import Navbar from './Navbar';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 
 //Pages
 import Home from './pages/Home';
 import Visualization from './pages/Visualization';
 
 function App() {
+  console.log(process.env.PUBLIC_URL);
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Navbar />
       <Switch>
-        <Route exact path={`/`} component={Home}></Route>
-        <Route exact path={`/visualization`} component={Visualization}></Route>
+        <Route exact path='/' component={Home}></Route>
+        <Route exact path='/visualization' component={Visualization}></Route>
       </Switch>
-    </Router>
+    </HashRouter>
   );
 }
 
