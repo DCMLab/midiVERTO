86d825e32f49af0464600c75cebbb844c6a72f05
AyTaken <39062949+AyTaken@users.noreply.github.com>
2021-10-14 10:17:53 +0200

    Added static analysis in application page

--- src/pages/Application.js
+++ src/pages/Application.js
@@ -1,11 +1,19 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Wavescape } from '../Wavescape';
+import WavescapeModule from '../WavescapeModule';
 import { prototypesData } from '../prototypesData';
-import Circle from '../Circle';
+import ResolutionSelector from '../ResolutionSelector';
+
 import Player, { setPlayerMidiData } from '../Player';
-import { getDftCoeffFromMidi, getRgbaMatrix } from '../getDftMatrices';
+import {
+  getDftCoeffFromMidi,
+  getRgbaMatrix,
+  getMidiFileDataObject,
+  getDftCoeffStatic,
+} from '../getDftMatrices';
 import dft from '../DFT';
 import parse from '../parser';
+
+import CoefficientsModule from '../CoefficientsModule';
 
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
@@ -15,169 +23,69 @@ import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
 
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
+import { Divider, Typography } from '@mui/material';
+
+let currentSongBPM = 80;
+let currentSongMidiData;
 
 function Application() {
-  //State: show or hide on the circles pitch class of the prototypes
-  const [selectedProtoPitchClasses, setSelectedProtoPitchClasses] =
-    useState(prototypesData);
-  const [showPrototypes, setShowPrototypes] = useState(true);
-
-  //State: contains the color data of the wavescape of a given coeff (Array(6) one for coeff)
-  const [wavescapesData, setWavescapesData] = useState([]);
-
   //State: represents the selected row on the wavescape (by default the first row) for each coeff
   const [tracesData, setTracesData] = useState([]);
   const [currentSubdiv, setCurrentSubdiv] = useState(0);
 
+  //State: contains the color data of the wavescape of a given coeff (Array(6) one for coeff)
+  const [wavescapesData, setWavescapesData] = useState([]);
+
+  const [resolutionMode, setResolutionMode] = useState({
+    noteValue: 0.25,
+    seconds: 1,
+    useSeconds: false,
+  });
+
   const [file, setFile] = useState('');
-
-  const [userPcvs, setUserPcvs] = useState([]);
-
-  const [resolution, setResolution] = useState(1);
-  const resolutionSliderRef = useRef(null);
-
-  const [multiRes, setMultiRes] = useState(1);
-  const [seconds, setSeconds] = useState(1);
-  const [useSeconds, setUseSeconds] = useState(false);
-  const resolutionTextRef = useRef(null);
-
-  function handleShowPrototypes(showing) {
-    let temp = selectedProtoPitchClasses.slice();
-
-    if (showing) temp.push(...prototypesData);
-    else temp = [];
-
-    setSelectedProtoPitchClasses(temp);
-    setShowPrototypes(showing);
-  }
 
   //MIDI parsing on file change
   useEffect(() => {
     let input = document.getElementById('file').files[0];
+
     if (input) {
-      //let resolution = +resolutionSliderRef.current.value;
       let fileReader = new FileReader();
       fileReader.readAsArrayBuffer(input);
-      fileReader.onload = (res) => {
-        //Once the file is loaded
+      fileReader.onload = (loadedFile) => {
+        let { midiData, midiBpm } = getMidiFileDataObject(
+          loadedFile.target.result
+        );
+        currentSongBPM = midiBpm;
+        currentSongMidiData = midiData;
 
-        //Computation of the minimum resolution given time-precision trade off
-        /* let mode = {
-          userFriendly: 150,
-          precise: 250,
-          scientific: 400,
-          stillBearable: 600,
-          goodLuck: 1000,
-        }; */
+        //setPlayerMidiData(currentSongMidiData, resolution, setCurrentSubdiv);
 
-        setPlayerMidiData(res.target.result, resolution, setCurrentSubdiv);
-        let dftCoeff = getDftCoeffFromMidi(res.target.result, resolution);
+        //Wavescapes static analysis
+        let subdivsNumberStatic = 50;
+        let staticResolution =
+          currentSongMidiData.duration / subdivsNumberStatic;
 
-        //Subdividing the first row of the dft coeff matrix to get the trace for each coeff
-        let traces = [];
-        let firstRow = dftCoeff[0];
-        for (let i = 1; i < 7; i++) {
-          let temp = [];
-          for (let j = 0; j < firstRow.length; j++) {
-            temp.push({ x: firstRow[j][i].re, y: firstRow[j][i].im });
-          }
-          traces.push(temp);
-        }
-        setTracesData(traces);
-        setWavescapesData(getRgbaMatrix(dftCoeff));
-        console.log(traces[0].length);
+        setWavescapesData(
+          getRgbaMatrix(
+            getDftCoeffStatic(currentSongMidiData, staticResolution)
+          )
+        );
+
+        //Circles dynamic analysis
       };
     }
   }, [file]);
 
-  const handleSubmit = (e) => {
-    //In order not to refresh the page (default behaviuor)
-    e.preventDefault();
-  };
-
-  const handleSubmitPitchClass = (e) => {
-    //In order not to refresh the page (default behaviuor)
-    e.preventDefault();
-    let parsedInput;
-
-    try {
-      parsedInput = parse(e.target[0].value);
-    } catch (error) {
-      console.log(error);
-      return;
-    }
-
-    let dftCoeffsInput = [];
-    for (let i = 0; i < parsedInput.length; i++) {
-      dftCoeffsInput.push(dft(parsedInput[i], true, true, false));
-    }
-
-    //Subdividing the coeffs for their coeff number
-    let subdivUserPcvs = [];
-    for (let i = 1; i < 7; i++) {
-      let temp = [];
-      for (let j = 0; j < dftCoeffsInput.length; j++) {
-        temp.push({ x: dftCoeffsInput[j][i].re, y: dftCoeffsInput[j][i].im });
-      }
-      subdivUserPcvs.push(temp);
-    }
-
-    if (userPcvs.length === 0) setUserPcvs(subdivUserPcvs);
-    else {
-      let temp = userPcvs.slice();
-      for (let i = 0; i < userPcvs.length; i++) {
-        temp[i].push(...subdivUserPcvs[i]);
-      }
-
-      setUserPcvs(temp);
-    }
-  };
-
-  function multiResOnChange(event) {
-    switch (event.target.value) {
-      case 'sixteenth':
-        setMultiRes(0.25);
-        setUseSeconds(false);
-        break;
-      case 'eighth':
-        setMultiRes(0.5);
-        setUseSeconds(false);
-        break;
-      case 'quarter':
-        setMultiRes(1);
-        setUseSeconds(false);
-        break;
-      case 'half':
-        setMultiRes(2);
-        setUseSeconds(false);
-        break;
-      case 'whole':
-        setMultiRes(4);
-        setUseSeconds(false);
-        break;
-      case 'seconds':
-        setUseSeconds(true);
-        let sec = setSeconds(parseFloat(resolutionTextRef.current.value));
-        break;
-      default:
-        setMultiRes(1);
-        break;
-    }
-  }
-
   return (
     <Container>
       <Player />
+
       <FormGroup>
         <FormControlLabel
           control={
             <Switch
-              onChange={() => handleShowPrototypes(!showPrototypes)}
-              checked={showPrototypes}
+            /* onChange={() => handleShowPrototypes(!showPrototypes)} */
+            /* checked={showPrototypes} */
             />
           }
           label='Show Prototypes'
@@ -214,92 +122,13 @@ function Application() {
         </label>
       </div>
 
-      <FormControl component='fieldset'>
-        <FormLabel component='legend'>Resolution</FormLabel>
-        <RadioGroup
-          row
-          defaultValue='quarter'
-          onChange={multiResOnChange}
-          aria-label='resolution'
-          name='row-radio-buttons-group'
-        >
-          <FormControlLabel
-            value='sixteenth'
-            control={<Radio />}
-            label=' &#119137;'
-          />
-          <FormControlLabel
-            value='eighth'
-            control={<Radio />}
-            label=' &#119136;'
-          />
-          <FormControlLabel
-            value='quarter'
-            control={<Radio />}
-            label='&#9833;'
-          />
-          <FormControlLabel
-            value='half'
-            control={<Radio />}
-            label='&#119134;'
-          />
-          <FormControlLabel
-            value='whole'
-            control={<Radio />}
-            label='&#119133;'
-          />
-        </RadioGroup>
-      </FormControl>
+      <ResolutionSelector />
 
       <Container>
-        <Circle
-          protoDataCoeff={selectedProtoPitchClasses[0]}
-          traceDataCoeff={tracesData[0]}
-          userPcvsCoeff={userPcvs[0]}
-          currentSubdiv={currentSubdiv}
-          performanceCoeff={{ x: 0, y: 0 }}
-        />
-        <Wavescape wavescapeMatrix={wavescapesData[0]} />
-        <Circle
-          protoDataCoeff={selectedProtoPitchClasses[1]}
-          traceDataCoeff={tracesData[1]}
-          userPcvsCoeff={userPcvs[1]}
-          currentSubdiv={currentSubdiv}
-          performanceCoeff={{ x: 0, y: 0 }}
-        />
-        <Wavescape wavescapeMatrix={wavescapesData[1]} />
-        <Circle
-          protoDataCoeff={selectedProtoPitchClasses[2]}
-          traceDataCoeff={tracesData[2]}
-          userPcvsCoeff={userPcvs[2]}
-          currentSubdiv={currentSubdiv}
-          performanceCoeff={{ x: 0, y: 0 }}
-        />
-        <Wavescape wavescapeMatrix={wavescapesData[2]} />
-        <Circle
-          protoDataCoeff={selectedProtoPitchClasses[3]}
-          traceDataCoeff={tracesData[3]}
-          userPcvsCoeff={userPcvs[3]}
-          currentSubdiv={currentSubdiv}
-          performanceCoeff={{ x: 0, y: 0 }}
-        />
-        <Wavescape wavescapeMatrix={wavescapesData[3]} />
-        <Circle
-          protoDataCoeff={selectedProtoPitchClasses[4]}
-          traceDataCoeff={tracesData[4]}
-          userPcvsCoeff={userPcvs[4]}
-          currentSubdiv={currentSubdiv}
-          performanceCoeff={{ x: 0, y: 0 }}
-        />
-        <Wavescape wavescapeMatrix={wavescapesData[4]} />
-        <Circle
-          protoDataCoeff={selectedProtoPitchClasses[5]}
-          traceDataCoeff={tracesData[5]}
-          userPcvsCoeff={userPcvs[5]}
-          currentSubdiv={currentSubdiv}
-          performanceCoeff={{ x: 0, y: 0 }}
-        />
-        <Wavescape wavescapeMatrix={wavescapesData[5]} />
+        <Divider />
+        <WavescapeModule wavescapesData={wavescapesData} />
+        <Divider />
+        <CoefficientsModule />
       </Container>
     </Container>
   );
