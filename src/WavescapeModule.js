9e6271aeae7e9c3a92cd9d4ba03a6ec2f97a40a9
AyTaken <39062949+AyTaken@users.noreply.github.com>
2021-10-15 12:01:07 +0200

    Added hide/show wavescape module

--- src/WavescapeModule.js
+++ src/WavescapeModule.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 
 import { Wavescape } from './Wavescape';
 
 import Box from '@mui/material/Box';
 import Switch from '@mui/material/Switch';
 import { Typography } from '@mui/material';
+import Collapse from '@mui/material/Collapse';
 
 function WavescapeModule({ wavescapesData }) {
+  const [showModule, setShowModule] = useState(false);
+
+  useEffect(() => {
+    if (wavescapesData.length > 0) setShowModule(true);
+  }, [wavescapesData]);
+
   return (
     <Box>
-      <Switch onChange={() => console.log('changed')} checked={true} />
+      <Switch
+        onChange={() => setShowModule(!showModule)}
+        checked={showModule}
+      />
+
       <Typography variant='h6'>Static Analysis</Typography>
-      {wavescapesData.map((data, i) => (
-        <Wavescape key={`w.${i}`} wavescapeMatrix={data} />
-      ))}
+      <Collapse in={showModule}>
+        {wavescapesData.map((data, i) => (
+          <Wavescape key={`w.${i}`} wavescapeMatrix={data} />
+        ))}
+      </Collapse>
     </Box>
   );
 }
