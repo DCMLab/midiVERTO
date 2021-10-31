5bbb84b401c34577bd34560b50c604164b4bf493
AyTaken <39062949+AyTaken@users.noreply.github.com>
2021-10-31 16:26:00 +0100

    Added custom theme

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const customTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#323232',
+    },
+    secondary: {
+      main: '#fefefe',
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={customTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
