62c51ff1417414d8b9aa46c1a0d1dfa42097874f
AyTaken <39062949+AyTaken@users.noreply.github.com>
2021-12-04 15:51:38 +0100

    Added markdown integration #29

---
+++ src/Markdown.js
@@ -0,0 +1,76 @@
+import * as React from 'react';
+import ReactMarkdown from 'markdown-to-jsx';
+import Typography from '@mui/material/Typography';
+/* import { Link } from 'react-router-dom'; */
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+
+function MarkdownListItem(props) {
+  return <Box component='li' sx={{ mt: 1, typography: 'body1' }} {...props} />;
+}
+
+function LinkReference({ to, ...props }) {
+  const myRef = React.useRef(null);
+
+  //Check for reference links, prevent default to not reload the page
+  //and scrollIntoView to focus on the footer when clicked
+  if (!isNaN(parseInt(props.href[1]))) {
+    return (
+      <Link
+        ref={myRef}
+        onClick={(event) => {
+          event.preventDefault();
+          myRef.current.scrollIntoView({
+            behavior: 'smooth',
+          });
+        }}
+        {...props}
+        id={`reference${props.href[1]}`}
+        href={`/#${props.href[1]}`}
+      />
+    );
+  } else return <Link {...props} />; //Else it is a normal external link
+}
+
+const options = {
+  overrides: {
+    h1: {
+      component: Typography,
+      props: {
+        gutterBottom: true,
+        variant: 'h4',
+        component: 'h1',
+      },
+    },
+    h2: {
+      component: Typography,
+      props: { gutterBottom: true, variant: 'h6', component: 'h2' },
+    },
+    h3: {
+      component: Typography,
+      props: { gutterBottom: true, variant: 'subtitle1' },
+    },
+    h4: {
+      component: Typography,
+      props: {
+        gutterBottom: true,
+        variant: 'caption',
+        paragraph: true,
+      },
+    },
+    p: {
+      component: Typography,
+      props: { paragraph: true },
+    },
+    a: {
+      component: LinkReference,
+    },
+    li: {
+      component: MarkdownListItem,
+    },
+  },
+};
+
+export default function Markdown(props) {
+  return <ReactMarkdown options={options} {...props} />;
+}
--- src/pages/Theory.js
+++ src/pages/Theory.js
@@ -1,104 +1,30 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Markdown from '../Markdown';
+import Box from '@mui/material/Box';
+import TheoryMarkdown from '../TheoryMarkdown.md';
 
 export default function Theory({ setOpen, setInAnalysisPage }) {
+  const [content, setContent] = useState(``);
+
   useEffect(() => {
     setInAnalysisPage(false);
     setOpen(false);
+
+    //Fetch the text of the markdown file as a string
+    fetch(TheoryMarkdown)
+      .then((response) => response.text())
+      .then((text) => {
+        setContent(text);
+      });
   }, []);
 
   return (
     <Paper sx={{ margin: 'auto', maxWidth: '1200px' }}>
-      <Typography variant='h3' sx={{ padding: 3, paddingBottom: 0 }}>
-        Theory page
-      </Typography>
-      <Accordion defaultExpanded disableGutters>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls='panel1a-content'
-          id='panel1a-header'
-        >
-          <Typography variant='h6'>Concept 1</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure,
-            officiis incidunt! Error, vitae. Facilis aperiam tenetur molestiae
-            animi velit nihil, laboriosam iste consequuntur ab. Ipsa, pariatur
-            quo hic ratione adipisci amet quibusdam eius quisquam. Laboriosam
-            odio voluptatum odit eius quo. Aperiam, nostrum perferendis? Iusto
-            sit labore eveniet, provident sequi consectetur quis explicabo
-            mollitia temporibus tempora asperiores. Animi nostrum dicta sapiente
-            dolorem nulla distinctio reiciendis? Saepe voluptates ea debitis
-            asperiores nam illo similique quidem! Delectus aut aperiam
-            consequuntur eveniet excepturi sapiente numquam similique
-            dignissimos, sit dolor id fuga vel illum optio eligendi cumque,
-            blanditiis illo debitis placeat dolorum commodi incidunt molestias!
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion defaultExpanded disableGutters>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls='panel2a-content'
-          id='panel2a-header'
-        >
-          <Typography variant='h6'>Concept 2</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo
-            inventore ab soluta molestias ad tempora libero, fuga cupiditate
-            totam magni nisi voluptatibus, corporis omnis! Aut explicabo, error
-            quasi enim perspiciatis facilis quibusdam magnam, provident eos
-            cumque consectetur necessitatibus ipsam ipsum! Nobis, exercitationem
-            sit? Nulla explicabo assumenda suscipit sequi repudiandae cupiditate
-            quos libero ab excepturi sapiente accusamus voluptatum minima rerum,
-            consequuntur quasi commodi dolorem eveniet cumque exercitationem!
-            Numquam maxime porro magnam ipsam? Ex molestiae autem rem dicta
-            iusto harum. Nam hic rem odio sed esse! Accusamus minus, laudantium
-            omnis aliquam est nobis ipsa tenetur nihil quia rem voluptatum
-            nesciunt doloribus, odit quasi nostrum. Maxime architecto quo
-            exercitationem quis repellat corrupti doloribus odit dolorum itaque
-            nisi ut quisquam natus, accusamus fugit voluptate harum at sed
-            repellendus eveniet iusto enim expedita totam dicta? Ratione,
-            quaerat! Vero porro velit nobis fuga? Quia, omnis ad. At odio
-            incidunt obcaecati, corporis, dolore neque alias, omnis facere ad
-            laborum vel animi culpa. Veniam tempore, veritatis quis neque rem
-            illo consequuntur aliquam ad obcaecati ipsam vitae tenetur itaque
-            temporibus dolor nisi cumque fugit? Suscipit porro totam, nemo illo
-            unde tempora eius, blanditiis repudiandae laboriosam adipisci quasi!
-            Consequatur, accusamus laborum officiis rerum placeat magni deserunt
-            ut impedit! Facere, illum!
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion defaultExpanded disableGutters>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls='panel3a-content'
-          id='panel3a-header'
-        >
-          <Typography variant='h6'>Concept 3</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            \Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
-            alias ullam in sed id iusto molestias vitae natus iure quae
-            repudiandae numquam voluptates necessitatibus, veniam perferendis
-            error sit ipsum dolor, nihil laboriosam voluptatum impedit tempora?
-            Quibusdam expedita pariatur molestias nemo, eaque, fugit nihil culpa
-            fugiat, ullam vero natus. Deleniti fuga unde dolorem asperiores
-            ipsam laudantium harum rem fugiat quas fugit.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
+      <Box sx={{ padding: 3 }}>
+        <Markdown>{content}</Markdown>
+      </Box>
     </Paper>
   );
 }
