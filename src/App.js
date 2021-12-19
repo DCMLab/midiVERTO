af069237dd707ac9deea78d887f1d646b8a1e624
AyTaken <39062949+AyTaken@users.noreply.github.com>
2021-12-19 11:21:56 +0100

    Commented code App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import Navbar from './Navbar';
+//React
 import { HashRouter, Route, Switch } from 'react-router-dom';
-import CssBaseline from '@mui/material/CssBaseline';
 import { useState, useEffect, useRef } from 'react';
 
+//Import functions
 import { setPlayerMidiData } from './Player';
 import {
-  getComplementaryColours,
   getRgbaMatrix,
   getMidiFileDataObject,
   getDftCoeffStatic,
@@ -15,16 +14,20 @@ import dft from './DFT';
 import parse from './parser';
 import { getRosePoints } from './roses';
 
+//Import components
+import Navbar from './Navbar';
 import ResolutionSelector from './ResolutionSelector';
 import PcvChipsBox from './PcvChipsBox';
+import SaveDialog from './SaveDialog';
 
-//Pages
+//Import pages
 import Home from './pages/Home';
 import Theory from './pages/Theory';
 import Analysis from './pages/Analysis';
 
-//Test
+//Import material UI components
 import { styled, useTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
@@ -34,14 +37,12 @@ import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Paper from '@mui/material/Paper';
 import Slider from '@mui/material/Slider';
-
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Divider from '@mui/material/Divider';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import MuiSwitch from '@mui/material/Switch';
-import SaveDialog from './SaveDialog';
 
+//Change this varible to change the size
+//of the Drawer component on the left
 const drawerWidth = 400;
 
 //Song variables
@@ -49,6 +50,7 @@ let currentSongBPM = 80;
 let currentSongMidiData;
 let fileName;
 
+//Drawer animation
 const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
   ({ theme, open }) => ({
     flexGrow: 1,
@@ -70,6 +72,7 @@ const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
   })
 );
 
+//Custom style for Drawer header
 const DrawerHeader = styled('div')(({ theme }) => ({
   display: 'flex',
   alignItems: 'center',
@@ -79,44 +82,89 @@ const DrawerHeader = styled('div')(({ theme }) => ({
   justifyContent: 'space-between',
 }));
 
+//Main component
 function App() {
   const theme = useTheme();
+
+  // ----- STATES ----- //
+
+  //GENERAL STATES
+
+  //State: boolean
+  //If true the Drawer is opened.
   const [open, setOpen] = useState(false);
-
+  //State: string
+  //File path MIDI.
   const [file, setFile] = useState('');
-  //State: contains the color data of the wavescape of a given coeff (Array(6) one for coeff)
+  //State: boolean
+  //Close or open the Drawer whenever the user move to another page.
+  const [inAnalysisPage, setInAnalysisPage] = useState(false);
+
+  //ANALYSIS STATES
+
+  //State: string[6][50]
+  //RGBA data of the wavescapes, from 1st to 6th coefficient.
   const [wavescapesData, setWavescapesData] = useState([]);
-
-  //State: represents the selected row on the wavescape (by default the first row) for each coeff
+  //State: points[6][N]
+  //Points in the Fourier spaces (from 1st to 6th) related
+  //to pcvs extraced from subdivisions with the selected resolution.
   const [coeffTracesData, setCoeffTracesData] = useState([]);
+  //State: points[6][N]
+  //Windowed version of coeffTracesData.
   const [windowedCoeffTraces, setWindowedCoeffTraces] = useState([]);
+  //State: integer
+  //Current subdivision being played by the MIDI player,
+  //second index of coeffTracesData[1:6][currentSubdiv].
   const [currentSubdiv, setCurrentSubdiv] = useState(0);
+  //State: integer
+  //Multiplicative factor of base time resolution.
   const [windowLen, setWindowLen] = useState(1);
-  const [invalidWndLen, setInvalidWndLen] = useState(false);
-  const [textfieldWndLen, setTextfieldWndLen] = useState('1');
-
-  //State: inputs
+  //State: object
+  //Changed only by ResolutionSelector component.
+  //noteResolutionValue: number, 1 is quarter note, 0.5 is eighth note...
+  //seconds: custom resolution in seconds
+  //useSeconds: use seconds attribute when true
   const [resolutionMode, setResolutionMode] = useState({
     noteResolutionValue: 1,
     seconds: 1.5,
     useSeconds: false,
   });
+  //State: number
+  //Computed in order to have always 50 subdivisions for the wavescape,
+  //trade-off between MIDI file duration and minimum resolution since
+  //computing wavescapes presents high time complexity.
   const [wavescapeResolution, setWavescapeResolution] = useState(10);
+  //State: number
+  //Can be controlled by the user. Resolution used to compute the
+  //coefficients in the Fourier spaces without computing the whole wavescape.
   const [circleResolution, setCircleResolution] = useState(1);
 
+  //INPUT STATES
+
+  //State: boolean
+  //True if the text field input of the window length is an invalid string.
+  const [invalidWndLen, setInvalidWndLen] = useState(false);
+  //State: string
+  //Current string in the text field input of the window length.
+  const [textfieldWndLen, setTextfieldWndLen] = useState('1');
+  //State: array
+  //Contains all the pcvs inserted by the user.
+  //label: string representing the pcv
+  //coeffs: points in the 6 Fourier spaces
+  //n,d: num and den, indexes of the rose matrix
+  //rosePoints: points that form the rose's shape
   const [userPcvs, setUserPcvs] = useState([]);
   const pcvTextRef = useRef(null);
-
-  //State: error states in input
+  //State: boolean
+  //If pcv input parser throw an error, this state is true --> show error
   const [isInputPcvInvalid, setIsInputPcvInvalid] = useState(false);
-
-  //State: in analysis page flag
-  const [inAnalysisPage, setInAnalysisPage] = useState(false);
-
-  //State: usable/used roses for pcvs' labels
+  //State: matrix
+  //Matrix that tracks if with a given index can be used or
+  //it is already in use.
   const [rosesMat, setRosesMat] = useState([]);
 
-  //When window length is changed, recompute the trace to be visualized
+  //Effect: when window length is changed,
+  //recompute the trace to be visualized
   useEffect(() => {
     let temp = [];
 
@@ -147,8 +195,9 @@ function App() {
     }
   }, [windowLen, coeffTracesData]);
 
+  //Pitch class vector submit function handler
   function handleSubmitPitchClass(input) {
-    //In order not to refresh the page (default behaviuor)
+    //Parse the input
     let parsedInput;
     let userPcvObjects = [];
 
@@ -160,6 +209,7 @@ function App() {
       return;
     }
 
+    //Create new pcv object
     parsedInput.forEach((input) =>
       userPcvObjects.push({
         label: `(${input})`,
@@ -194,15 +244,21 @@ function App() {
           }
         }
       }
+
+      //If the are no available roses, return without changing anything
+      if (!found) return;
     });
 
+    //Update the state with the new pcvs
     setUserPcvs([...userPcvs, ...userPcvObjects]);
   }
 
-  //MIDI parsing on file change
+  //Effect: MIDI analysis on file change
   useEffect(() => {
+    //Get the uploaded file
     let input = document.getElementById('file').files[0];
 
+    //Parse the MIDI file
     if (input) {
       let fileReader = new FileReader();
       fileReader.readAsArrayBuffer(input);
@@ -212,11 +268,12 @@ function App() {
             loadedFile.target.result
           );
 
+          //Set song variables
           currentSongBPM = midiBpm;
           currentSongMidiData = midiData;
           fileName = input.name;
 
-          //Wavescapes static analysis
+          //Wavescapes "static" analysis --> 50 subdivs
           let subdivsNumberStatic = 50;
           let staticResolution =
             currentSongMidiData.duration / subdivsNumberStatic;
@@ -228,7 +285,7 @@ function App() {
             )
           );
 
-          //Circles dynamic analysis
+          //Circles "dynamic" analysis --> single traces in the Fourier spaces
           let { tracesData, resolution } = getDftCoeffDynamic(
             midiData,
             resolutionMode,
@@ -240,7 +297,7 @@ function App() {
           setWindowLen(1);
           setTextfieldWndLen('1');
 
-          console.log(midiData);
+          //console.log(midiData);
         } catch (error) {
           console.log(error);
           return;
@@ -249,8 +306,9 @@ function App() {
     }
   }, [file]);
 
-  //Init roseMatrix and user pcv examples
+  //Effect: init roseMatrix and user pcv examples
   useEffect(() => {
+    //7x7 matrix
     let initMatrix = [];
     for (let i = 1; i <= 7; i++) {
       let temp = [];
@@ -270,6 +328,7 @@ function App() {
 
     setRosesMat(initMatrix);
 
+    //Examples
     let examplePcvs = [
       [1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0], // C
       [1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0], // Cmin
@@ -304,16 +363,19 @@ function App() {
       }
     });
 
+    //2 disabled pcvs to show that there is the possibility to disable them
     exPcvObjects[1].isDisabled = true;
     exPcvObjects[2].isDisabled = true;
 
     setUserPcvs(exPcvObjects);
   }, []);
 
+  //Close Drawer function handler
   const handleDrawerClose = () => {
     setOpen(false);
   };
 
+  //Re-compute the analysis when clicking on "Change" button in the Drawer
   function retriggerAnalysis() {
     //Only if there is midi data, retrigger analysis
     if (currentSongMidiData == null) {
@@ -336,13 +398,16 @@ function App() {
   return (
     <HashRouter>
       <CssBaseline />
+
       <Navbar
         open={open}
         setOpen={setOpen}
         setInAnalysisPage={setInAnalysisPage}
         inAnalysisPage={inAnalysisPage}
       />
+
       <Box sx={{ display: 'flex' }}>
+        {/* DRAWER */}
         <Drawer
           sx={{
             width: drawerWidth,
@@ -356,6 +421,7 @@ function App() {
           anchor='left'
           open={open}
         >
+          {/* DRAWER HEADER  */}
           <DrawerHeader>
             <Stack sx={{ flexGrow: '2', maxWidth: '85%', marginBottom: 1 }}>
               <Typography sx={{ fontWeight: 'bold' }}>
@@ -390,7 +456,6 @@ function App() {
                 </Typography>
               </Stack>
             </Stack>
-
             <IconButton onClick={handleDrawerClose}>
               {theme.direction === 'ltr' ? (
                 <ChevronLeftIcon />
@@ -400,6 +465,8 @@ function App() {
             </IconButton>
           </DrawerHeader>
           <Divider />
+
+          {/* TIME RESOLUTION MODULE */}
           <Typography sx={{ marginLeft: 1, marginTop: 1, fontWeight: 'bold' }}>
             Time resolution
           </Typography>
@@ -419,6 +486,8 @@ function App() {
             />
           </Box>
           <Divider />
+
+          {/* WINDOW LENGTH MODULE */}
           <Typography
             sx={{
               marginLeft: 1,
@@ -441,7 +510,7 @@ function App() {
               }}
               variant='h5'
               gutterBottom
-            >{`\u00D7 `}</Typography>
+            >{`=`}</Typography>
             <TextField
               inputProps={{ style: { textAlign: 'center' } }}
               sx={{ width: '15%' }}
@@ -478,7 +547,7 @@ function App() {
               variant='h5'
               gutterBottom
             >
-              {'time resolution'}
+              {'\u00D7 time resolution'}
             </Typography>
           </Stack>
           <Slider
@@ -515,6 +584,8 @@ function App() {
             }
           />
           <Divider />
+
+          {/* PCV USER INPUT MODULE */}
           <Typography sx={{ marginLeft: 1, marginTop: 1, fontWeight: 'bold' }}>
             Custom pitch-class vectors
           </Typography>
@@ -560,6 +631,8 @@ function App() {
             />
           </Box>
           <Divider />
+
+          {/* LEGEND OF COEFFICIENTS */}
           <Typography sx={{ marginLeft: 1, marginTop: 1, fontWeight: 'bold' }}>
             Legend of coefficients
           </Typography>
@@ -654,12 +727,16 @@ function App() {
               </Stack>
             </Paper>
           </Box>
+
+          {/* EXPORT MODULE */}
           <SaveDialog
             traces={windowedCoeffTraces}
             userPcvs={userPcvs}
             wavescapesData={wavescapesData}
           />
         </Drawer>
+
+        {/* MAIN */}
         <Main open={open}>
           <Switch>
             <Route
@@ -695,6 +772,8 @@ function App() {
                   fullTraces={coeffTracesData}
                   coeffTracesData={windowedCoeffTraces}
                   currentSubdiv={
+                    //Mapping between coeffTracesData's subdivs
+                    //and windowedCoeffTraces' subdivs --> centerd window
                     windowedCoeffTraces.length > 0 &&
                     Math.floor(windowLen / 2) + currentSubdiv >=
                       windowedCoeffTraces[0].length
