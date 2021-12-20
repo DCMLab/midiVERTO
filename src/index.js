a82cc00c139c35fac14c9372ced042d0c7d765df
AyTaken <39062949+AyTaken@users.noreply.github.com>
2021-12-20 12:56:23 +0100

    Commented code index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+//React
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+//Import components
 import App from './App';
+
+//Import material UI components
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+//Custom theme
 const customTheme = createTheme({
   palette: {
     primary: {
