b2a0d58b67e0082a319fca054f3ae3eee127db93
AyTaken <39062949+AyTaken@users.noreply.github.com>
2021-12-20 09:50:21 +0100

    Commented code Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -1,24 +1,33 @@
+//React
 import React from 'react';
 
+//Import images
 import logo from './logos/placeholder.png';
 
+//Import material UI components
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
 
+//Footer component
 function Footer() {
   let logoSize = 100;
   return (
     <Box sx={{ marginTop: 1, textAlign: 'center' }}>
+      {/* LOGOS */}
       <Stack sx={{ justifyContent: 'center' }} direction='row'>
         <img width={logoSize} height={logoSize} src={logo} />
         <img width={logoSize} height={logoSize} src={logo} />
         <img width={logoSize} height={logoSize} src={logo} />
       </Stack>
+
+      {/* TEXT */}
       <Typography>
         Project “Enabling interactive music visualization for a wider community”
       </Typography>
+
+      {/* PEOPLE */}
       <Stack sx={{ justifyContent: 'center' }} direction='row'>
         <Typography sx={{ marginRight: 1 }}>
           Project lead: Fabian C. Moss; Daniel Harasim
@@ -28,6 +37,8 @@ function Footer() {
           Developer: Giovanni Affatato
         </Typography>
       </Stack>
+
+      {/* LINKS */}
       <Stack sx={{ justifyContent: 'center' }} direction='row'>
         <Link sx={{ marginRight: 1 }} href='https://github.com/DCMLab/MIDFT'>
           GitHub repo
