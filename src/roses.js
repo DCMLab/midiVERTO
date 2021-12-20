7da684ed47faf4bad35dd52cf2f28a8eb51de8bf
AyTaken <39062949+AyTaken@users.noreply.github.com>
2021-12-20 13:28:36 +0100

    Commented roses.js

--- src/roses.js
+++ src/roses.js
@@ -1,6 +1,14 @@
+/**
+ * Compute the points of the rose's shape
+ * see https://en.wikipedia.org/wiki/Rose_(mathematics)
+ * @param {number} n numerator
+ * @param {number} d denominator
+ * @returns array
+ */
 export function getRosePoints(n, d) {
   let dataPoints = [];
 
+  //Increase delta step to have less points
   for (let delta = 0; delta < 2 * Math.PI * d; delta += 0.01) {
     let r = 10 * Math.cos((n / d) * delta);
     let x = r * Math.cos(delta);
